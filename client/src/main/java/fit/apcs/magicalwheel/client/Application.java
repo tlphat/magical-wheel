cc3900e79b20e57ba37706ce3ac4350089a50f88
Phat Tang <tlphat18@apcs.vn>
2021-10-21 07:09:28 +0700

    init project
    Setup project with two modules: client and server

---
+++ client/src/main/java/fit/apcs/magicalwheel/client/Application.java
@@ -0,0 +1,9 @@
+package fit.apcs.magicalwheel.client;
+
+public final class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Client up");
+    }
+
+}
---
+++ server/src/main/java/fit/apcs/magicalwheel/server/Application.java
@@ -0,0 +1,9 @@
+package fit.apcs.magicalwheel.server;
+
+public final class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Server up");
+    }
+
+}
