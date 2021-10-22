0919307d280c7115ef9d5f02bdeec37d01552e83
Phat Tang <tlphat18@apcs.vn>
2021-10-22 17:53:28 +0700

    init class player

---
+++ server/src/main/java/fit/apcs/magicalwheel/server/enitty/Player.java
@@ -0,0 +1,45 @@
+package fit.apcs.magicalwheel.server.enitty;
+
+public class Player {
+
+    private final String name;
+    private int point; // = 0 when first initialized
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (obj == this) {
+            return true;
+        }
+        return obj instanceof Player && ((Player) obj).name.equals(name);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("{%s, %d}", name, point);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void increasePoint(int increasedBy) {
+        point += increasedBy;
+    }
+
+}
