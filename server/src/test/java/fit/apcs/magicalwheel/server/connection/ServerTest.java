2637b394f89d44e9f2f509778d48acc0dcf7b0b8
Phat Tang <tlphat18@apcs.vn>
2021-10-31 18:54:15 +0700

    server: add test multiple connections for Server class

---
+++ server/src/test/java/fit/apcs/magicalwheel/server/connection/ServerTest.java
@@ -0,0 +1,49 @@
+package fit.apcs.magicalwheel.server.connection;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ServerTest {
+
+    private Thread serverThread;
+
+    @BeforeEach
+    void startServerThread() {
+        serverThread = new Thread(() -> Server.getInstance().run());
+        serverThread.start();
+    }
+
+    @AfterEach
+    void waitForServerThread() throws InterruptedException {
+        serverThread.join();
+    }
+
+    @Test
+    void testMultipleConnections() {
+        assertDoesNotThrow(() -> {
+            final var socketList = IntStream.range(0, 100).mapToObj(i -> {
+                try {
+                    return SocketChannel.open(new InetSocketAddress("127.0.0.1", 8080));
+                } catch (IOException e) {
+                    throw new RuntimeException("Cannot connect to server socket");
+                }
+            }).collect(Collectors.toList());
+            for (int id = 0; id < 100; ++id) {
+                final var channel = socketList.get(id);
+                channel.write(ByteBuffer.wrap(String.valueOf(id).getBytes(StandardCharsets.UTF_8)));
+            }
+        });
+    }
+
+}
