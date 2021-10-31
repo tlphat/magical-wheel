adc8ac949e4a435fe2489375c0292d6fd56ca4ce
Phat Tang <tlphat18@apcs.vn>
2021-10-31 13:44:10 +0700

    server: temporarily change to non thread-safe data structures
    Next commit should fix the issue of concurrency

--- server/src/main/java/fit/apcs/magicalwheel/server/gameplay/GamePlay.java
+++ server/src/main/java/fit/apcs/magicalwheel/server/gameplay/GamePlay.java
@@ -1,7 +1,7 @@
 package fit.apcs.magicalwheel.server.gameplay;
 
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.ArrayList;
+import java.util.List;
 
 import fit.apcs.magicalwheel.server.entity.Player;
 import fit.apcs.magicalwheel.server.entity.Question;
@@ -10,23 +10,22 @@ public class GamePlay {
 
     private static final int MAX_NUM_PLAYERS = 2;
 
-    private final ConcurrentHashMap<Player, Integer> mapPlayerOrder = new ConcurrentHashMap<>();
+    private final List<Player> players = new ArrayList<>();
     private final Question question = GameLoader.getInstance().getRandomQuestion();
-    private final AtomicInteger currentNumPlayer = new AtomicInteger(0);
 
     public synchronized boolean canStart() {
-        return mapPlayerOrder.size() == MAX_NUM_PLAYERS;
+        return players.size() == MAX_NUM_PLAYERS;
     }
 
     public synchronized void addPlayer(Player player) {
         if (canStart()) {
             throw new UnsupportedOperationException("There is enough players got connected");
         }
-        if (mapPlayerOrder.containsKey(player)) {
+        if (players.contains(player)) {
             throw new IllegalArgumentException(
                     String.format("Player with username %s is already existed", player.getName()));
         }
-        mapPlayerOrder.put(player, currentNumPlayer.incrementAndGet());
+        players.add(player);
     }
 
     public void start() {
