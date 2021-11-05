e69b1d3595682f667cc2bbc169a10265a54c303a
Dung Pham <pltdunggg@gmail.com>
2021-11-05 21:30:39 +0700

    client: refactor code

--- client/src/main/java/fit/apcs/magicalwheel/client/view/MainFrame.java
+++ client/src/main/java/fit/apcs/magicalwheel/client/view/MainFrame.java
@@ -1,29 +1,14 @@
 package fit.apcs.magicalwheel.client.view;
 
 import java.awt.Color;
-import java.awt.Font;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.IOException;
 import java.io.Serial;
-import java.net.URL;
-import java.util.Optional;
-import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.regex.Pattern;
 
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.SwingConstants;
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
 
 import fit.apcs.magicalwheel.client.connection.Client;
 
@@ -33,20 +18,15 @@ public class MainFrame extends JFrame {
     private static final long serialVersionUID = 7641338013687300073L;
     private static final Logger LOGGER = Logger.getLogger(MainFrame.class.getName());
 
-    private static final String GAME_NAME = "Magical Wheel";
-
-    private JButton playButton;
-    private JTextField usernameField;
-    private JLabel message;
 
     public MainFrame() {
-        setTitle(GAME_NAME);
+        setTitle(ResourceUtil.GAME_NAME);
         setOnExitEvent();
         setExtendedState(MAXIMIZED_BOTH);
         setResizable(false);
-        setIconImage(new ImageIcon(getImageURL("wheel.png")).getImage());
+        setIconImage(new ImageIcon(ResourceUtil.getImageURL("wheel.png")).getImage());
         getContentPane().setBackground(Color.BLACK);
-        addMainPanel();
+        addWelcomePanel();
         setVisible(true);
     }
 
@@ -65,116 +45,8 @@ public class MainFrame extends JFrame {
         });
     }
 
-    private void addMainPanel() {
-        final var mainPanel = new JPanel();
-
-        // for display purpose only (to guarantee that this text always takes 1 line)
-        message = new JLabel(" ");
-        message.setForeground(Color.WHITE);
-
-        mainPanel.setOpaque(false);
-        mainPanel.setLayout(new GridBagLayout());
-
-        final var gbc = new GridBagConstraints();
-        gbc.gridwidth = GridBagConstraints.REMAINDER;
-        gbc.anchor = GridBagConstraints.NORTH;
-        gbc.insets = new Insets(15,0,0,0);
-
-        mainPanel.add(centralLabel(), gbc);
-        mainPanel.add(usernameField(), gbc);
-        mainPanel.add(playButton(), gbc);
-        mainPanel.add(message, gbc);
-
-        add(mainPanel);
+    private void addWelcomePanel() {
+        final var welcomePanel = new WelcomePanel();
+        add(welcomePanel);
     }
-
-    private JPanel usernameField() {
-        final var usernamePanel = new JPanel();
-        final var usernameLabel = new JLabel("Enter username: ");
-
-        handleUsernameField();
-
-        usernameLabel.setForeground(Color.WHITE);
-        usernamePanel.setOpaque(false);
-        usernamePanel.add(usernameLabel);
-        usernamePanel.add(usernameField);
-
-        return usernamePanel;
-    }
-
-    private void handleUsernameField() {
-        usernameField = new JTextField(10);
-        usernameField.setDocument(new JTextFieldLimit(10));
-        usernameField.getDocument().addDocumentListener(new DocumentListener() {
-
-            @Override
-            public void insertUpdate(DocumentEvent e) {
-                resetButtonAndMessage();   
-            }
-
-            @Override
-            public void removeUpdate(DocumentEvent e) {
-                resetButtonAndMessage();
-            }
-
-            @Override
-            public void changedUpdate(DocumentEvent e) {
-                resetButtonAndMessage();
-            }
-
-            public void resetButtonAndMessage() {
-                message.setText(" ");
-                playButton.setEnabled(true);
-            }
-
-        });
-    }
-
-    private JButton playButton() {
-        playButton = new JButton();
-        playButton.addActionListener(event -> onPlayButtonClickListener());
-        playButton.setText("PLAY");
-        playButton.setHorizontalAlignment(SwingConstants.CENTER);
-        playButton.setVerticalAlignment(SwingConstants.CENTER);
-        return playButton;
-    }
-
-    private void onPlayButtonClickListener() {
-        playButton.setEnabled(false);
-        final var username = usernameField.getText();
-        if (isUsernameVerified(username)) {
-            LOGGER.log(Level.INFO, "Button clicked");
-            final var client = Client.getInstance();
-            client.openConnection(unused -> client.sendUsername(username));
-        }
-    }
-
-    private boolean isUsernameVerified(String username) {
-        final var pattern = "^[a-zA-Z0-9_]+$";
-        if (!Pattern.matches(pattern, username)) {
-            message.setText("Username must be composed by 'a'..'z', 'A'..'Z', '0'..'9', '_'");
-            return false;
-        }
-        return true;
-    }
-
-    private JLabel centralLabel() {
-        final var label = new JLabel();
-        label.setText(GAME_NAME);
-        label.setIcon(new ImageIcon(getImageURL("wheel.png")));
-        label.setHorizontalTextPosition(SwingConstants.CENTER);
-        label.setVerticalTextPosition(SwingConstants.TOP);
-        label.setForeground(Color.YELLOW);
-        label.setFont(new Font("Goudy Old Style", Font.BOLD, 50));
-        label.setIconTextGap(20);
-        label.setHorizontalAlignment(SwingConstants.CENTER);
-        label.setVerticalAlignment(SwingConstants.CENTER);
-        return label;
-    }
-
-    private URL getImageURL(String imageName) {
-        return Optional.ofNullable(getClass().getClassLoader().getResource(imageName))
-                       .orElseThrow(() -> new RuntimeException("Cannot find image " + imageName));
-    }
-
 }
---
+++ client/src/main/java/fit/apcs/magicalwheel/client/view/ResourceUtil.java
@@ -0,0 +1,19 @@
+package fit.apcs.magicalwheel.client.view;
+
+import java.net.URL;
+import java.util.Optional;
+
+public final class ResourceUtil {
+
+    public static final String GAME_NAME = "Magical Wheel";
+
+
+    private ResourceUtil() {
+
+    }
+
+    public static URL getImageURL(String imageName) {
+        return Optional.ofNullable(ResourceUtil.class.getClassLoader().getResource(imageName))
+                       .orElseThrow(() -> new RuntimeException("Cannot find image " + imageName));
+    }
+}
