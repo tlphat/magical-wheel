cdcbc024c6ad2b4bf3af6934ea1b8704e45ae032
Phat Tang <tlphat18@apcs.vn>
2021-11-06 09:40:04 +0700

    client: add string field for enum

--- client/src/main/java/fit/apcs/magicalwheel/client/constant/EventType.java
+++ client/src/main/java/fit/apcs/magicalwheel/client/constant/EventType.java
@@ -1,10 +1,41 @@
 package fit.apcs.magicalwheel.client.constant;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Stream;
+
+import javax.annotation.Nullable;
+
 public enum EventType {
-    JOIN_ROOM,
-    NEW_PLAYER,
-    START_GAME,
-    PLAYER_GUESS,
-    START_TURN,
-    END_GAME
+
+    JOIN_ROOM("1"),
+    NEW_PLAYER("2"),
+    START_GAME("3"),
+    PLAYER_GUESS("4"),
+    START_TURN("5"),
+    END_GAME("6");
+
+    private static final Logger LOGGER = Logger.getLogger(EventType.class.getName());
+    private final String value;
+
+    EventType(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Nullable
+    @SuppressWarnings("ReturnOfNull")
+    public static EventType fromString(String str) {
+        return Stream.of(values())
+                     .filter(s -> s.getValue().equalsIgnoreCase(str))
+                     .findFirst()
+                     .orElseGet(() -> {
+                         LOGGER.log(Level.WARNING, "Cannot map {0} to EventType", str);
+                         return null;
+                     });
+    }
+
 }
