29f4468061ed7d3405848d9a8e7b171f6cff17fa
Phat Tang <tlphat18@apcs.vn>
2021-11-06 21:48:37 +0700

    client: refactor ResourceUtil
    remove unused gbc field

--- client/src/main/java/fit/apcs/magicalwheel/client/view/ResourceUtil.java
+++ client/src/main/java/fit/apcs/magicalwheel/client/view/ResourceUtil.java
@@ -1,20 +1,19 @@
 package fit.apcs.magicalwheel.client.view;
 
-import java.awt.GridBagConstraints;
 import java.net.URL;
 import java.util.Optional;
 
 public final class ResourceUtil {
 
     public static final String GAME_NAME = "Magical Wheel";
-    public static GridBagConstraints gbc;
 
     private ResourceUtil() {
-        gbc = new GridBagConstraints();
+
     }
 
     public static URL getImageURL(String imageName) {
         return Optional.ofNullable(ResourceUtil.class.getClassLoader().getResource(imageName))
                        .orElseThrow(() -> new RuntimeException("Cannot find image " + imageName));
     }
+
 }
