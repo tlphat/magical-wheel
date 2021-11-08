7b761e8315fc0177b800b285039e9bf0ee1fb15c
Phat Tang <tlphat18@apcs.vn>
2021-11-08 12:10:58 +0700

    server: separate join game handler

--- server/src/main/java/fit/apcs/magicalwheel/server/connection/Server.java
+++ server/src/main/java/fit/apcs/magicalwheel/server/connection/Server.java
@@ -1,18 +1,7 @@
 package fit.apcs.magicalwheel.server.connection;
 
-import static fit.apcs.magicalwheel.lib.constant.EventType.JOIN_ROOM;
-import static fit.apcs.magicalwheel.lib.constant.EventType.fromString;
-import static fit.apcs.magicalwheel.lib.constant.StatusCode.FULL_CONNECTION;
-import static fit.apcs.magicalwheel.lib.constant.StatusCode.OK;
-import static fit.apcs.magicalwheel.lib.constant.StatusCode.SERVER_ERROR;
-import static fit.apcs.magicalwheel.lib.constant.StatusCode.USERNAME_EXISTED;
-import static fit.apcs.magicalwheel.lib.constant.StatusCode.USERNAME_INVALID;
-import static fit.apcs.magicalwheel.lib.constant.StatusCode.WRONG_FORMAT;
-import static fit.apcs.magicalwheel.lib.util.SocketWriteUtil.getMessageFromLines;
-import static fit.apcs.magicalwheel.lib.util.SocketWriteUtil.writeStringToChannel;
 import static java.util.concurrent.TimeUnit.SECONDS;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
@@ -21,12 +10,9 @@ import java.nio.channels.AsynchronousSocketChannel;
 import java.nio.channels.CompletionHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.stream.Stream;
 
 import fit.apcs.magicalwheel.lib.util.SocketReadUtil;
-import fit.apcs.magicalwheel.server.entity.Player;
-import fit.apcs.magicalwheel.server.exception.DuplicatedResourceException;
-import fit.apcs.magicalwheel.server.exception.WrongMessageFormatException;
+import fit.apcs.magicalwheel.server.connection.handler.JoinGameHandler;
 import fit.apcs.magicalwheel.server.gameplay.GamePlay;
 
 public final class Server {
@@ -45,7 +31,7 @@ public final class Server {
         return INSTANCE;
     }
 
-    private static void closeClientChannel(AsynchronousSocketChannel channel) {
+    public static void closeClientChannel(AsynchronousSocketChannel channel) {
         try {
             channel.close();
         } catch (IOException ex) {
@@ -92,53 +78,7 @@ public final class Server {
 
     private void readUsername(AsynchronousSocketChannel clientChannel) {
         final var byteBuffer = ByteBuffer.allocate(200);
-        // TODO: separate completion handler into a new class
-        final var completionHandler = new CompletionHandler<Integer, Void>() {
-            @Override
-            public void completed(Integer numBytes, Void attachment) {
-                try {
-                    final var reader = SocketReadUtil.byteBufferToReader(byteBuffer, numBytes);
-                    validateEventType(reader);
-                    final var username = reader.readLine().trim();
-                    final var player = new Player(username, clientChannel);
-                    final var players = gamePlay.addPlayer(player);
-                    final var responseBody =
-                            Stream.concat(Stream.of(OK.getCode(), GamePlay.MAX_NUM_PLAYERS, players.size()),
-                                          players.stream().map(Player::getUsername)).toArray(Object[]::new);
-                    writeStringToChannel(clientChannel, getMessageFromLines(JOIN_ROOM, responseBody));
-                } catch (UnsupportedOperationException ex) { // TODO: simplify and generalize this logic
-                    writeStringToChannel(clientChannel,
-                                         getMessageFromLines(JOIN_ROOM, FULL_CONNECTION.getCode()), false);
-                } catch (DuplicatedResourceException ex) {
-                    writeStringToChannel(clientChannel,
-                                         getMessageFromLines(JOIN_ROOM, USERNAME_EXISTED.getCode()), false);
-                } catch (WrongMessageFormatException ex) {
-                    writeStringToChannel(clientChannel,
-                                         getMessageFromLines(JOIN_ROOM, WRONG_FORMAT.getCode()), false);
-                } catch (IllegalArgumentException ex) {
-                    writeStringToChannel(clientChannel,
-                                         getMessageFromLines(JOIN_ROOM, USERNAME_INVALID.getCode()), false);
-                } catch (IOException | RuntimeException ex) {
-                    writeStringToChannel(clientChannel,
-                                         getMessageFromLines(JOIN_ROOM, SERVER_ERROR.getCode()), false);
-                }
-            }
-
-            private void validateEventType(BufferedReader reader) throws IOException {
-                final var type = fromString(reader.readLine());
-                if (type != JOIN_ROOM) {
-                    LOGGER.log(Level.WARNING, "Expect response of type {0}, got {1}",
-                               new Object[]{ JOIN_ROOM, type});
-                    throw new WrongMessageFormatException("Wrong event type");
-                }
-            }
-
-            @Override
-            public void failed(Throwable exc, Void attachment) {
-                LOGGER.log(Level.WARNING, "Timeout waiting for name", exc);
-                closeClientChannel(clientChannel);
-            }
-        };
+        final var completionHandler = new JoinGameHandler(byteBuffer, clientChannel, gamePlay);
         clientChannel.read(byteBuffer, SocketReadUtil.TIMEOUT_IN_SECONDS, SECONDS, null, completionHandler);
     }
 
---
+++ server/src/main/java/fit/apcs/magicalwheel/server/connection/handler/JoinGameHandler.java
@@ -0,0 +1,89 @@
+package fit.apcs.magicalwheel.server.connection.handler;
+
+import static fit.apcs.magicalwheel.lib.constant.EventType.JOIN_ROOM;
+import static fit.apcs.magicalwheel.lib.constant.EventType.fromString;
+import static fit.apcs.magicalwheel.lib.constant.StatusCode.FULL_CONNECTION;
+import static fit.apcs.magicalwheel.lib.constant.StatusCode.OK;
+import static fit.apcs.magicalwheel.lib.constant.StatusCode.SERVER_ERROR;
+import static fit.apcs.magicalwheel.lib.constant.StatusCode.USERNAME_EXISTED;
+import static fit.apcs.magicalwheel.lib.constant.StatusCode.USERNAME_INVALID;
+import static fit.apcs.magicalwheel.lib.constant.StatusCode.WRONG_FORMAT;
+import static fit.apcs.magicalwheel.lib.util.SocketWriteUtil.getMessageFromLines;
+import static fit.apcs.magicalwheel.lib.util.SocketWriteUtil.writeStringToChannel;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.nio.channels.CompletionHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Stream;
+
+import fit.apcs.magicalwheel.lib.util.SocketReadUtil;
+import fit.apcs.magicalwheel.server.connection.Server;
+import fit.apcs.magicalwheel.server.entity.Player;
+import fit.apcs.magicalwheel.server.exception.DuplicatedResourceException;
+import fit.apcs.magicalwheel.server.exception.WrongMessageFormatException;
+import fit.apcs.magicalwheel.server.gameplay.GamePlay;
+
+public class JoinGameHandler implements CompletionHandler<Integer, Void> {
+
+    private static final Logger LOGGER = Logger.getLogger(JoinGameHandler.class.getName());
+    private final ByteBuffer byteBuffer;
+    private final AsynchronousSocketChannel clientChannel;
+    private final GamePlay gamePlay;
+
+    public JoinGameHandler(ByteBuffer byteBuffer, AsynchronousSocketChannel clientChannel, GamePlay gamePlay) {
+        this.byteBuffer = byteBuffer;
+        this.clientChannel = clientChannel;
+        this.gamePlay = gamePlay;
+    }
+
+    @Override
+    public void completed(Integer numBytes, Void attachment) {
+        try {
+            final var reader = SocketReadUtil.byteBufferToReader(byteBuffer, numBytes);
+            validateEventType(reader);
+            final var username = reader.readLine().trim();
+            final var player = new Player(username, clientChannel);
+            final var players = gamePlay.addPlayer(player);
+            final var responseBody =
+                    Stream.concat(Stream.of(OK.getCode(), GamePlay.MAX_NUM_PLAYERS, players.size()),
+                                  players.stream().map(Player::getUsername)).toArray(Object[]::new);
+            writeStringToChannel(clientChannel, getMessageFromLines(JOIN_ROOM, responseBody));
+            // TODO: announce new player has joined room
+        } catch (UnsupportedOperationException ex) { // TODO: simplify and generalize this logic
+            writeStringToChannel(clientChannel,
+                                 getMessageFromLines(JOIN_ROOM, FULL_CONNECTION.getCode()), false);
+        } catch (DuplicatedResourceException ex) {
+            writeStringToChannel(clientChannel,
+                                 getMessageFromLines(JOIN_ROOM, USERNAME_EXISTED.getCode()), false);
+        } catch (WrongMessageFormatException ex) {
+            writeStringToChannel(clientChannel,
+                                 getMessageFromLines(JOIN_ROOM, WRONG_FORMAT.getCode()), false);
+        } catch (IllegalArgumentException ex) {
+            writeStringToChannel(clientChannel,
+                                 getMessageFromLines(JOIN_ROOM, USERNAME_INVALID.getCode()), false);
+        } catch (IOException | RuntimeException ex) {
+            writeStringToChannel(clientChannel,
+                                 getMessageFromLines(JOIN_ROOM, SERVER_ERROR.getCode()), false);
+        }
+    }
+
+    private static void validateEventType(BufferedReader reader) throws IOException {
+        final var type = fromString(reader.readLine());
+        if (type != JOIN_ROOM) {
+            LOGGER.log(Level.WARNING, "Expect response of type {0}, got {1}",
+                       new Object[]{ JOIN_ROOM, type});
+            throw new WrongMessageFormatException("Wrong event type");
+        }
+    }
+
+    @Override
+    public void failed(Throwable exc, Void attachment) {
+        LOGGER.log(Level.WARNING, "Timeout waiting for name", exc);
+        Server.closeClientChannel(clientChannel);
+    }
+
+}
