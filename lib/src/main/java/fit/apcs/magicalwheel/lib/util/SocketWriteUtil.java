3e913c920c7dd266d13ebb42f935e89b5b812639
Phat Tang <tlphat18@apcs.vn>
2021-11-08 11:07:33 +0700

    lib: add new write method that can config keep alive

--- lib/src/main/java/fit/apcs/magicalwheel/lib/util/SocketWriteUtil.java
+++ lib/src/main/java/fit/apcs/magicalwheel/lib/util/SocketWriteUtil.java
@@ -1,13 +1,19 @@
 package fit.apcs.magicalwheel.lib.util;
 
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.AsynchronousSocketChannel;
+import java.nio.channels.CompletionHandler;
 import java.nio.charset.StandardCharsets;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import fit.apcs.magicalwheel.lib.constant.EventType;
 
 // TODO: add java doc for this utility class
 public final class SocketWriteUtil {
+
+    private static final Logger LOGGER = Logger.getLogger(SocketWriteUtil.class.getName());
 
     private SocketWriteUtil() {
 
@@ -23,6 +29,30 @@ public final class SocketWriteUtil {
 
     public static void writeStringToChannel(AsynchronousSocketChannel channel, String message) {
         channel.write(ByteBuffer.wrap(message.getBytes(StandardCharsets.UTF_8)));
+    }
+
+    public static void writeStringToChannel(AsynchronousSocketChannel channel,
+                                            String message, boolean keepAlive) {
+        if (keepAlive) {
+            writeStringToChannel(channel, message);
+            return;
+        }
+        channel.write(ByteBuffer.wrap(message.getBytes(StandardCharsets.UTF_8)), null,
+                      new CompletionHandler<Integer, Void>() {
+            @Override
+            public void completed(Integer result, Void attachment) {
+                try {
+                    channel.close();
+                } catch (IOException ex) {
+                    LOGGER.log(Level.SEVERE, "Error in closing connection", ex);
+                }
+            }
+
+            @Override
+            public void failed(Throwable ex, Void attachment) {
+                LOGGER.log(Level.SEVERE, "Error in sending message", ex);
+            }
+        });
     }
 
 }
