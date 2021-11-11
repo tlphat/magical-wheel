e76aa4dc85887405a68fd51b9f4174974ca61baf
Phat Tang <tlphat18@apcs.vn>
2021-11-11 14:53:11 +0700

    client: refactor GameInfoPanel
    * remove redundant imports
    * add serial version
    * replace 10 by keywordLength
    * replace JLabel constant by SwingConstants

--- client/src/main/java/fit/apcs/magicalwheel/client/view/panel/GameInfoPanel.java
+++ client/src/main/java/fit/apcs/magicalwheel/client/view/panel/GameInfoPanel.java
@@ -1,20 +1,21 @@
 package fit.apcs.magicalwheel.client.view.panel;
 
 import java.awt.BorderLayout;
-import java.awt.Component;
-import java.awt.Font;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.GridLayout;
 import java.awt.Insets;
+import java.io.Serial;
 
-import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.border.Border;
+import javax.swing.SwingConstants;
 
 public class GameInfoPanel extends JPanel {
+
+    @Serial
+    private static final long serialVersionUID = 1113033786957875666L;
 
     private final JLabel keywordLabel;
     private final JLabel hintLabel;
@@ -23,7 +24,7 @@ public class GameInfoPanel extends JPanel {
         keywordLabel = new JLabel();
         hintLabel = new JLabel();
         hintLabel.setText("<html><p style=\"width:350px\">" + hint + "</p></html>");
-        setNewKeyword(new String(new char[10]).replace('\0', '*'));
+        setNewKeyword(new String(new char[keywordLength]).replace('\0', '*'));
         setOpaque(false);
         setLayout(new BorderLayout());
         add(keywordPanel(), BorderLayout.WEST);
@@ -33,6 +34,7 @@ public class GameInfoPanel extends JPanel {
         keywordLabel.setText(keywordFormat(keyword));
     }
 
+    @SuppressWarnings("MethodMayBeStatic")
     private String keywordFormat(String keyword) {
         return keyword;
     }
@@ -60,6 +62,7 @@ public class GameInfoPanel extends JPanel {
     private void keywordLabelDecor() {
         keywordLabel.setForeground(Color.WHITE);
         keywordLabel.setFont(new Font("Courier New", Font.BOLD, 20));
-        keywordLabel.setHorizontalAlignment(JLabel.CENTER);
+        keywordLabel.setHorizontalAlignment(SwingConstants.CENTER);
     }
+
 }
