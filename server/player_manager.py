8c3782aeaeab8263a26bd7b4cb17bd875915aa85
Minh-Thao Ha <hdmthao@akas-MacBook.local>
2021-11-17 20:13:47 +0700

    fix some logic
    Signed-off-by: Minh-Thao Ha <hdmthao@akas-MacBook.local>

--- server/player_manager.py
+++ server/player_manager.py
@@ -6,18 +6,49 @@ class PlayerManager:
         self.game = game
         self.cur_player_idx = -1
         self.cur_player = None
+        self.next_player = None
         self.players = []
         self.num_player = 0
 
-    def get_next_player(self):
-        posible_next_player_idx = (self.cur_player_idx + 1) % (self.num_player)
+    def has_next_player(self):
+        posible_next_player_idx = self.get_player_idx(self.cur_player) if self.cur_player else 0
 
         count = 0
         while count < self.num_player:
             if not self.players[posible_next_player_idx].eliminated():
-                self.cur_player_idx = posible_next_player_idx
-                self.cur_player = self.players[self.cur_player_idx]
-                return self.players[self.cur_player_idx]
+                return True
+
+            posible_next_player_idx += 1
+            posible_next_player_idx %= self.num_player
+            count += 1
+
+        return False
+
+    def set_next_player(self, player):
+        self.next_player = player
+
+    def get_player_idx(self, player):
+        for idx, p in enumerate(self.players):
+            if p == player:
+                return idx
+
+        return None
+
+    def get_next_player(self):
+        if self.next_player and not self.next_player.eliminated():
+            self.cur_player = self.next_player
+            self.next_player = None
+            return self.cur_player
+        else:
+            self.next_player = None
+
+        posible_next_player_idx = self.get_player_idx(self.cur_player) if self.cur_player else 0
+
+        count = 0
+        while count < self.num_player:
+            if not self.players[posible_next_player_idx].eliminated():
+                self.cur_player = self.players[posible_next_player_idx]
+                return self.cur_player
 
             posible_next_player_idx += 1
             posible_next_player_idx %= self.num_player
