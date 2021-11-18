49b7c9066bddae79de9282f5222f15f891ef47f8
Thuy-Dung Pham <50902849+dungplt242@users.noreply.github.com>
2021-11-18 15:05:50 +0700

    client: character indicator layout (#14)
    * client: fix submit panel
    
    * client: fix submit panel
    
    * client: update game panel
    
    * client: minor refactor
    
    * type in method name
    * redundant imports
    * add blank line
    
    * client: plugin logic from guess reponse handler
    
    Co-authored-by: Phat Tang <tlphat18@apcs.vn>

--- client/src/main/java/fit/apcs/magicalwheel/client/view/panel/SubmitPanel.java
+++ client/src/main/java/fit/apcs/magicalwheel/client/view/panel/SubmitPanel.java
@@ -1,6 +1,7 @@
 package fit.apcs.magicalwheel.client.view.panel;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
@@ -11,7 +12,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.Nullable;
-import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -46,20 +46,26 @@ public class SubmitPanel extends JPanel {
             characterButtons.add(characterButton(c));
         }
 
-        setLayout(new GridLayout(2, 1));
-        setOpaque(false);
-        add(characterPanel());
-        add(keywordPanel());
+        initLayout();
         disableSubmission(false);
+    }
+
+    private void initLayout() {
+        final var gbc = new GridBagConstraints();
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        setLayout(new GridBagLayout());
+        setOpaque(false);
+        setBackground(Color.BLUE);
+        add(characterPanel(), gbc);
+        add(keywordPanel(), gbc);
     }
 
     private JButton characterButton(char c) {
         final var button = new JButton();
         button.setText(Character.toString(c));
-        button.setMargin(new Insets(5, 5, 5, 5));
+        button.setMargin(new Insets(2, 2, 2, 2));
         button.setForeground(Color.WHITE);
         button.setBackground(Color.BLACK);
-        button.setBorder(BorderFactory.createLineBorder(Color.WHITE));
         button.addActionListener(this::onCharButtonClickListener);
         button.setFocusPainted(false);
         return button;
@@ -87,12 +93,13 @@ public class SubmitPanel extends JPanel {
     private JPanel keywordPanel() {
         final var panel = new JPanel(new GridBagLayout());
         final var gbc = new GridBagConstraints();
-        final var label = new JLabel("Keyword guess (optional): ");
-        keywordField.setColumns(10);
+        final var label = new JLabel("Keyword (optional): ");
+        keywordFieldDecor();
         submitButton.setText("Submit");
         submitButton.addActionListener(event -> onSubmitButtonListener());
         label.setForeground(Color.WHITE);
-        gbc.insets = new Insets(10, 0, 0, 0);
+        label.setFont(new Font("Calibri", Font.PLAIN, 15));
+        gbc.insets = new Insets(10, 10, 10, 10);
         gbc.anchor = GridBagConstraints.LINE_START;
         gbc.gridx = 0;
         gbc.gridy = 0;
@@ -100,11 +107,21 @@ public class SubmitPanel extends JPanel {
         gbc.gridx = 1;
         gbc.gridy = 0;
         panel.add(keywordField, gbc);
-        gbc.gridx = 1;
+        gbc.gridx = 0;
         gbc.gridy = 1;
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        gbc.anchor = GridBagConstraints.NORTH;
         panel.add(submitButton, gbc);
         panel.setOpaque(false);
         return panel;
+    }
+
+    private void keywordFieldDecor() {
+        keywordField.setColumns(10);
+        keywordField.setBackground(Color.BLACK);
+        keywordField.setForeground(Color.WHITE);
+        keywordField.setCaretColor(Color.WHITE);
+        keywordField.setFont(new Font("Calibri", Font.PLAIN, 15));
     }
 
     private void onSubmitButtonListener() {
@@ -122,8 +139,8 @@ public class SubmitPanel extends JPanel {
     private JPanel characterPanel() {
         final var panel = new JPanel();
         final var layout = new GridLayout(2, 13);
-        layout.setHgap(2);
-        layout.setVgap(2);
+        layout.setHgap(3);
+        layout.setVgap(3);
         panel.setLayout(layout);
         panel.setOpaque(false);
         for (JButton button: characterButtons) {
@@ -143,4 +160,5 @@ public class SubmitPanel extends JPanel {
     public void disableKeywordField() {
         keywordField.setEnabled(false);
     }
+
 }
